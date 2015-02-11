dd77e0167d058904100c7a30f374c07b9cb23b22
Witoo Harianto <witoo@outlook.com>
2015-02-11 10:23:14 +0700

    add pongo2

---
+++ pongo2/pongo2.go
@@ -0,0 +1,31 @@
+package pongo2
+
+import (
+	"github.com/plimble/ace"
+	"github.com/plimble/copter"
+	"net/http"
+)
+
+type TemplateOptions struct {
+	Directory     string
+	Extensions    []string
+	IsDevelopment bool
+}
+
+type pongo2 struct {
+	copter *copter.Copter
+}
+
+func (p *pongo2) Render(w http.ResponseWriter, name string, data interface{}) {
+	p.copter.ExecW(name, data, w)
+}
+
+func Pongo2(options *TemplateOptions) ace.Renderer {
+	return &pongo2{
+		copter: copter.New(&copter.Options{
+			Directory:     options.Directory,
+			Extensions:    options.Extensions,
+			IsDevelopment: options.IsDevelopment,
+		}),
+	}
+}
